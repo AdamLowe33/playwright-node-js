b79bd4e75769dcf4262bac2ba41244a5115fe76a
Adam Lowe <adamlowe@ip-10-0-0-68.us-west-2.compute.internal>
2025-10-15 13:28:08 -0600

    adding buildspec file

--- tests/lambdaApiReturnsHello.spec.js
+++ tests/lambdaApiReturnsHello.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('lambda API returns hello', async ({ request }) => {
-    const response = await request.get(`https://<api-gateway-id>.execute-api.us-east-1.amazonaws.com/dev/hello`);
+    const response = await request.get(`https://us-east-2.console.aws.amazon.com/apigateway/main/apis/qji4iohy0c/stages?api=qji4iohy0c&region=us-east-2`);
     expect(response.ok()).toBeTruthy();
 
     const data = await response.json();
